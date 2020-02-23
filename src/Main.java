2e548755c1fc1cfc1978ca9e1bcaa684d5d77b81
hamzab97 <hbana3@uwo.ca>
2020-02-23 17:59:51 -0500

    change path

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
         disjointSet = new DisjointSet();
-        calculateDimensions("./src/girl.img");
+        calculateDimensions("girl.img");
     }
 
     /*
